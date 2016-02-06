1916784c3c738602f122097bd3c033eb2a83f9fb
Maxime <Maxime@192.168.1.92>
2016-02-06 21:17:35 +0100

    Move / Create Directory /

--- src/files/FileFactory.java
+++ src/files/FileFactory.java
@@ -1,19 +1,43 @@
 package files;
 
-import java.util.*;
 import java.io.*;
+
+import static java.nio.file.StandardCopyOption.*;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class FileFactory {
 	
-	// LE GPS NEGRO
+	// Create a file
 	public static void CreateFile(String path) {
 		try {
 		      File file = new File(path);
 		      if(!file.createNewFile()){ 
 		    	  System.out.println("Erreur : Le fichier " + path + " existe déjà !");
 		      }
-	    	} catch (IOException e) {
+	    	} catch(IOException e) {
 		      e.printStackTrace();
+		}
+	}
+	
+	// Create a directory
+	public static void CreateDirectory(String path) {
+		File dir = new File(path);
+		// if the directory does not exist, create it
+		if(dir.exists()) {
+		    System.out.println("Erreur : Le dossier " + path + " existe déjà !");     
+		}
+		else {
+			try {
+		        dir.mkdir();
+		        System.out.println("ok!");
+		    } 
+		    catch(SecurityException se){
+		    	 se.printStackTrace();
+		    }    
 		}
 	}
 	
@@ -27,7 +51,7 @@ public class FileFactory {
 	        reader.read(chars);
 	        output = new String(chars);
 	        reader.close();
-	    } catch (IOException e) {
+	    } catch(IOException e) {
 	        e.printStackTrace();
 	    }
 		return output;
@@ -39,10 +63,33 @@ public class FileFactory {
 		  try {
 		      writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path), "utf-8"));
 		      writer.write(input);
-		  } catch (IOException ex) {
+		  } catch(IOException ex) {
 			  ex.printStackTrace();
 		  } finally {
 		     try {writer.close();} catch (Exception ex) {}
 		  }
+	}
+	
+	// Move a file
+	public static void Move(String pathFrom, String pathTo) {
+		try {
+			Path fileFrom = Paths.get(pathFrom);
+			Path fileTo = Paths.get(pathTo);
+			Files.move(fileFrom, fileTo, REPLACE_EXISTING);
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+	
+	// Delete a file
+	public static void Delete(String path) {
+		Path file = Paths.get(path);
+		try {
+		  Files.deleteIfExists(file);
+		} catch(DirectoryNotEmptyException dnee) {
+		  System.err.println("Le repertoire " + file + " n'est pas vide");
+		} catch(IOException ioe) {
+		  System.err.println("Impossible de supprimer " + file + " : " + ioe);
+		}
 	}
 }
--- src/main/main.java
+++ src/main/main.java
@@ -6,15 +6,16 @@ public class main {
 
 	public static void main(String[] args) {
 			
-		String file1 = "toto.txt";
+		String dir1 = "totot";
+		String file1 = dir1 + "/" + "totott.txt";
+		String dir2 = "toto-rasse.txt";
 		
+		FileFactory.CreateDirectory(dir1);
 		FileFactory.CreateFile(file1);
 		FileFactory.Write(file1, "salut rasse");
 		String output = FileFactory.Read(file1);
+		FileFactory.Move(file1, dir2);
 		System.out.println(output);
-
-		//bsartek ça compile
-
 	}
 
 }
