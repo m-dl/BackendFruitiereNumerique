fb46ce27ad8b7e4884e135b199dba3fff8280a00
Maxime <Maxime@PC>
2016-02-27 12:33:01 +0100

    entities package

--- src/entities/InterestPoint.java
+++ src/entities/InterestPoint.java
@@ -1,7 +1,38 @@
 package entities;
 
+import java.util.ArrayList;
+
 /**
  * Created by adriansalas on 08/02/2016.
  */
 public class InterestPoint {
+  private ArrayList<Media> M;
+  private String name;
+  
+  public InterestPoint(String name) {
+    this.setM(new ArrayList<Media>());
+    this.name = name;
+  }
+
+  /**
+   * @return the M
+   */
+  public ArrayList<Media> getM() {
+    return M;
+  }
+
+  /**
+   * @param M the M to set
+   */
+  public void setM(ArrayList<Media> M) {
+    this.M = M;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
--- src/entities/Visit.java
+++ src/entities/Visit.java
@@ -1,7 +1,38 @@
 package entities;
 
+import java.util.ArrayList;
+
 /**
  * Created by adriansalas on 08/02/2016.
  */
 public class Visit {
+  private ArrayList<InterestPoint> IP;
+  private String name;
+  
+  public Visit(String name) {
+    this.setIP(new ArrayList<InterestPoint>());
+    this.name = name;
+  }
+
+  /**
+   * @return the iP
+   */
+  public ArrayList<InterestPoint> getIP() {
+    return IP;
+  }
+
+  /**
+   * @param iP the iP to set
+   */
+  public void setIP(ArrayList<InterestPoint> IP) {
+    this.IP = IP;
+  }
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
