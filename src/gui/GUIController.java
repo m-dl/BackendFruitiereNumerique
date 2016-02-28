79a470353a104a34bf0ce612ac5afa4cf5916abe
Adrian Salas <adrian.salas84@gmail.com>
2016-02-28 18:16:30 +0100

    - ajout gui

---
+++ src/gui/GUIController.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+public class GUIController {
+
+    @FXML
+    void uploadMedia() {
+        System.out.println("uploadMedia");
+    }
+
+    @FXML
+    void downloadMedia() {
+        System.out.println("downloadMedia");
+    }
+
+    @FXML
+    void delOld() {
+        System.out.println("delOld");
+    }
+
+
+}
---
+++ src/gui/GUIWindow.java
@@ -0,0 +1,24 @@
+package gui;/**
+ * Created by adriansalas on 28/02/2016.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUIWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Ajout de fichiers");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
