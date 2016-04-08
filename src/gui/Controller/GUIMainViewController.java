72b15bff921b486b0ddb3d69e229b18649c6d9d6
Adrian <adrian.salas84@gmail.com>
2016-04-09 00:36:58 +0200

    - bouton upload/download + test sur une barre de progression : pas au point ;(

--- src/gui/Controller/GUIMainViewController.java
+++ src/gui/Controller/GUIMainViewController.java
@@ -1,7 +1,8 @@
 package gui.Controller;
 
-import gui.GUIWindow;
+import files.FileManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TabPane;
 
 public class GUIMainViewController {
@@ -10,6 +11,8 @@ public class GUIMainViewController {
 
     @FXML
     private TabPane tabPane;
+    @FXML
+    private ProgressBar progressBar; // TODO: 09/04/2016 trouver un moyen de set progress pendant dl
 
     private GUIMainViewController()
     {}
@@ -18,14 +21,29 @@ public class GUIMainViewController {
         return INSTANCE;
     }
 
+    // TODO: 08/04/2016 Faut des threads (ça bloque l'affichage javafx aussi )
     @FXML
     void uploadMedia() {
-        System.out.println("uploadMedia");
+        progressBar.setVisible(true);
+        if (getTabPane().getSelectionModel().getSelectedIndex() == 0) {
+            System.out.println("uploadMedia chateau");
+            FileManager.getInstance().uploadToDriveChateau();
+        } else if (getTabPane().getSelectionModel().getSelectedIndex() == 1) {
+            System.out.println("uploadMedia village");
+            FileManager.getInstance().uploadToDriveVillage();
+        }
     }
 
+    // TODO: 08/04/2016 Faut des threads ici aussi
     @FXML
     void downloadMedia() {
-        System.out.println("downloadMedia");
+        if (getTabPane().getSelectionModel().getSelectedIndex() == 0) {
+            System.out.println("downloadMedia chateau");
+            FileManager.getInstance().downloadFromDriveChateau();
+        } else if (getTabPane().getSelectionModel().getSelectedIndex() == 1) {
+            System.out.println("downloadMedia village");
+            FileManager.getInstance().downloadFromDriveVillage();
+        }
     }
 
 
