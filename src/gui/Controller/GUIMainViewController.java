0f338edc4c21db0d80b06177461eaa62d05728e6
Adrian <adrian.salas84@gmail.com>
2016-04-09 14:39:20 +0200

    - tests

--- src/gui/Controller/GUIMainViewController.java
+++ src/gui/Controller/GUIMainViewController.java
@@ -1,18 +1,26 @@
 package gui.Controller;
 
 import files.FileManager;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TabPane;
 
 public class GUIMainViewController {
 
     private static GUIMainViewController INSTANCE = new GUIMainViewController();
-
+    Task<Void> downloadMedia = new Task<Void>() {
+        @Override
+        protected Void call() throws Exception {
+            //FileManager.getInstance().uploadToDriveChateau();
+            FileManager.getInstance().uploadToDriveVillage();
+            return null;
+        }
+    };
     @FXML
     private TabPane tabPane;
     @FXML
-    private ProgressBar progressBar; // TODO: 09/04/2016 trouver un moyen de set progress pendant dl
+    private ProgressIndicator progressIndicator; // TODO: 09/04/2016 trouver un moyen de set progress pendant dl
 
     private GUIMainViewController()
     {}
@@ -24,10 +32,9 @@ public class GUIMainViewController {
     // TODO: 08/04/2016 Faut des threads (ça bloque l'affichage javafx aussi )
     @FXML
     void uploadMedia() {
-        progressBar.setVisible(true);
         if (getTabPane().getSelectionModel().getSelectedIndex() == 0) {
             System.out.println("uploadMedia chateau");
-            FileManager.getInstance().uploadToDriveChateau();
+            new Thread(downloadMedia).start();
         } else if (getTabPane().getSelectionModel().getSelectedIndex() == 1) {
             System.out.println("uploadMedia village");
             FileManager.getInstance().uploadToDriveVillage();
@@ -45,7 +52,6 @@ public class GUIMainViewController {
             FileManager.getInstance().downloadFromDriveVillage();
         }
     }
-
 
     public TabPane getTabPane() {
         return tabPane;
